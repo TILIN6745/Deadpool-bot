eb2311939defcedb901ca99d0bf1f231adc77f00
Tilin <jm293229@gmail.com>
2025-03-05 16:15:41 -0600

    Update config.js

--- config.js
+++ config.js
@@ -12,12 +12,12 @@ import { en, es, id, ar, pt } from "./lib/idiomas/total-idiomas.js"
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 // [ES] > Agrega el número que será Bot y los que serán propietarios.
 global.owner = [
-["5218336105471", 'Tilin', true], //el tilin
+["5216181870830", 'Tilin', true], //el tilin
 ["5218336105471", 'gay', true], //es gay
-["5218336105471"]]
+["5216181870830"]]
 
-global.mods = ["5218336105471"]
-global.prems = ["5218336105471"]
+global.mods = ["5216181870830"]
+global.prems = ["5216181870830"]
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
@@ -112,9 +112,9 @@ global.moment = moment
 // [ES] > Agregate a ti, colaboradores o ayudates, aparecerá en el comando de lista de contactos.
 // [EN] > Adding yourself, collaborators or helpers will appear in the contact list command.
 global.official = [ // Agregate si eres Owner
-["5218336105471", 'tilin', 1], 
-["5218336105471", 'tilinn', 1],  
-["5218336105471", 'Tilins', 1]]
+["5216181870830", 'tilin', 1], 
+["5216181870830", 'tilinn', 1],  
+["5216181870830", 'Tilins', 1]]
 
 global.mail = '' // Add email
 global.desc = '' // Add short description (20 caractres max)
@@ -122,8 +122,8 @@ global.desc2 = '' // Add long description (90 caractres max) (Este parámetro se
 global.country = '' // Add country, example: 🇪🇨
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 
-global.packname = "𝗧𝗜𝗟𝗜𝗡-𝗕𝗢𝗧"
-global.author = "𝗧𝗜𝗟𝗜𝗡-𝗕𝗢𝗧"
+global.packname = "𝗗𝗘𝗔𝗗𝗣𝗢𝗢𝗟 𝗕𝗢𝗧"
+global.author = "𝗗𝗘𝗔𝗗𝗣𝗢𝗢𝗟 𝗕𝗢𝗧"
 
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 // [ES] > INFORMACIÓN DE VERSIONES DEL BOT, POR FAVOR 
@@ -131,7 +131,7 @@ global.author = "𝗧𝗜𝗟𝗜𝗡-𝗕𝗢𝗧"
 // [EN] > BOT VERSION INFORMATION, PLEASE KEEP THIS UNCHANGED, WE STRIVE DAILY TO PROVIDE YOU WITH A BOT FOR THE COMMUNITY, BE GRATEFUL
 global.vs = "1.7.0"
 global.vsJB = "2.5 (Beta)"
-global.gt = "𝗧𝗜𝗟𝗜𝗡-𝗕𝗢𝗧"
+global.gt = "𝗗𝗘𝗔𝗗𝗣𝗢𝗢𝗟 𝗕𝗢𝗧"
 
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 
@@ -157,9 +157,9 @@ global.eg = '╰⊱💚⊱ *𝙀́𝙓𝙄𝙏𝙊 | 𝙎𝙐𝘾𝘾𝙀𝙎�
 global.exito = eg
 
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
-global.wm = "𝗧𝗜𝗟𝗜𝗡-𝗕𝗢𝗧"
-global.igfg = "𝗧𝗜𝗟𝗜𝗡-𝗕𝗢𝗧"
-global.nomorown = "5218336105471"
+global.wm = "𝗗𝗘𝗔𝗗𝗣𝗢𝗢𝗟 𝗕𝗢𝗧"
+global.igfg = "𝗗𝗘𝗔𝗗𝗣𝗢𝗢𝗟 𝗕𝗢𝗧"
+global.nomorown = "5216181870830"
 global.pdoc = ["application/vnd.openxmlformats-officedocument.presentationml.presentation", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "application/vnd.ms-excel", "application/msword", "application/pdf", "text/rtf"]
 // • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • • •
 
